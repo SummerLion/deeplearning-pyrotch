3607af74871b96b982c577dc216e7ba4630b99bc
孙伟东 <1348805983@qq.com>
2022-08-03 09:05:43 +0800

    # 循环单元nn.RNNCell(input_size,hidden_size)

--- RNN/practise.py
+++ RNN/practise.py
@@ -1,14 +1,19 @@
 import torch
 import torch.nn as nn
+
 # 实例化一个rnn对象
 # 第一个参数：input_size(输入张量x的维度)
 # 第二个参数：hidden_size（隐藏层的维度，隐藏层神经元数量）
 # 第三个参数：num_layers(隐藏层的层数)
 basic_rnn = nn.RNN(input_size=20, hidden_size=50, num_layers=2)
 basic_rnn.weight_ih_l0
-print(basic_rnn.weight_ih_l0.shape)
-print(basic_rnn.weight_ih_l1.shape)
-print(basic_rnn.bias_ih_l0.shape)
-print(basic_rnn.bias_ih_l1.shape)
-print(basic_rnn.bias_hh_l0.shape)
-print(basic_rnn.bias_hh_l1.shape)
+# print(basic_rnn.weight_ih_l0.shape)
+# print(basic_rnn.weight_ih_l1.shape)
+# print(basic_rnn.bias_ih_l0.shape)
+# print(basic_rnn.bias_ih_l1.shape)
+# print(basic_rnn.bias_hh_l0.shape)
+# print(basic_rnn.bias_hh_l1.shape)
+
+cell = nn.RNNCell(input_size=3,hidden_size=5)
+for name,param in cell.named_parameters():
+    print('{} = {}'.format(name,param))
