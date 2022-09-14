398ebc6bbfce2f1fac1b6d2d6b86875fdbf8236b
孙伟东 <1348805983@qq.com>
2022-09-14 15:18:50 +0800

    # RNN做图像分类，该文件可以执行，并且在测试集上的准确率能达到95% Epoch 4. Train Loss: 0.215348, Train Acc: 0.936040, Valid Loss: 0.196332, Valid Acc: 0.937793, Time 00:00:23 Epoch 5. Train Loss: 0.168189, Train Acc: 0.950199, Valid Loss: 0.152052, Valid Acc: 0.954199, Time 00:00:23 Epoch 6. Train Loss: 0.140889, Train Acc: 0.958017, Valid Loss: 0.140624, Valid Acc: 0.956445, Time 00:00:23

--- RNN/rnn-for-image.py
+++ RNN/rnn-for-image.py
@@ -1,30 +1,39 @@
-import sys
-sys.path.append('..')
+# RNN 做图像分类
+# import sys
+# sys.path.append('..')
 
 import torch
-from torch.autograd import Variable
 from torch import nn
 from torch.utils.data import DataLoader
-
 from torchvision import transforms as tfs
 from torchvision.datasets import MNIST
-# 定义数据
+from datetime import datetime
+
+from tqdm import tqdm
+import os
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# 准备数据
 data_tf = tfs.Compose([
     tfs.ToTensor(),
-    tfs.Normalize([0.5], [0.5]) # 标准化
+    tfs.Normalize([0.5], [0.5])  # 标准化
 ])
-
-train_set = MNIST('../data', train=True, transform=data_tf,download=True)
-test_set = MNIST('../data', train=False, transform=data_tf)
-
-train_data = DataLoader(train_set, 64, True, num_workers=4)
-test_data = DataLoader(test_set, 128, False, num_workers=4)
+# 训练集
+train_set = MNIST(root='../data', train=True, download=False, transform=data_tf)
+# print('------>', len(train_set))
+# print(train_set[0])  # (<PIL.Image.Image image mode=L size=28x28 at 0x27B5813B640>, 5)
+test_set = MNIST(root='../data', train=False, transform=data_tf)
+test_dataloader = DataLoader(test_set, batch_size=256, shuffle=True)
+dataloader = DataLoader(train_set, batch_size=128, shuffle=True)
+# print('------>', len(dataloader))
+# for index, item in enumerate(dataloader):
+#         print(index)
 
 
 # 定义模型
-class rnn_classify(nn.Module):
+class RNN_classify(nn.Module):
     def __init__(self, in_feature=28, hidden_feature=100, num_class=10, num_layers=2):
-        super(rnn_classify, self).__init__()
+        super(RNN_classify, self).__init__()
         self.rnn = nn.LSTM(in_feature, hidden_feature, num_layers)  # 使用两层 lstm
         self.classifier = nn.Linear(hidden_feature, num_class)  # 将最后一个 rnn 的输出使用全连接得到最后的分类结果
 
@@ -39,10 +48,72 @@ class rnn_classify(nn.Module):
         out = self.classifier(out)  # 得到分类结果
         return out
 
-net = rnn_classify()
+
+model = RNN_classify().to(device)
+optimizer = torch.optim.Adadelta(model.parameters(), 1e-1)
+if os.path.exists('./model/model.pkl'):
+    model.load_state_dict(torch.load('./model/model.pkl'))
+if os.path.exists('./model/optimizer.pkl'):
+    optimizer.load_state_dict(torch.load('./model/optimizer.pkl'))
 criterion = nn.CrossEntropyLoss()
 
-optimzier = torch.optim.Adadelta(net.parameters(), 1e-1)
+
 # 开始训练
+def get_acc(output, label):
+    total = output.shape[0]
+    _, pred_label = output.max(1)
+    num_correct = (pred_label == label).sum().item()
+    return num_correct / total
+
+
 from utils import train
-train(net, train_data, test_data, 10, optimzier, criterion)
+train(model, dataloader, test_dataloader, 10, optimizer, criterion)
+
+# if __name__ == '__main__':
+#     prev_time = datetime.now()
+#     for epoch in tqdm(range(10), desc='训练：'):
+#         train_loss = 0
+#         train_acc = 0
+#         model = model.train()
+#
+#         for index, (im, label) in enumerate(dataloader):
+#             im = im.to(device)
+#             label = label.to(device)
+#             # forward
+#             output = model(im)
+#             loss = criterion(output, label)
+#             # backward
+#             optimizer.zero_grad()
+#             loss.backward()
+#             optimizer.step()
+#
+#             train_loss += loss.item()
+#             train_acc += get_acc(output, label)
+#
+#         valid_loss = 0
+#         valid_acc = 0
+#         model = model.eval()
+#         for im, label in test_dataloader:
+#             im = im.to(device)
+#             label = label.to(device)
+#
+#             output = model(im)
+#             loss = criterion(output, label)
+#             valid_loss += loss.item()
+#             valid_acc += get_acc(output, label)
+#         epoch_str = (
+#             "Epoch %d. Train Loss: %f, Train Acc: %f, Valid Loss: %f, Valid Acc: %f, "
+#             % (epoch, train_loss / len(dataloader),
+#                train_acc / len(dataloader), valid_loss / len(test_dataloader),
+#                valid_acc / len(test_dataloader)))
+#         print(epoch_str)
+#     torch.save(model.state_dict(), './model/model.pkl')
+#     torch.save(optimizer.state_dict(), './model/optimizer.pkl')
+#
+#     cur_time = datetime.now()
+#     h, remainder = divmod((cur_time - prev_time).seconds, 3600)
+#     m, s = divmod(remainder, 60)
+#     time_str = "Time %02d:%02d:%02d" % (h, m, s)
+#     print(time_str)
+
+
